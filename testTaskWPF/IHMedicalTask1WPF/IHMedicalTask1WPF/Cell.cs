4a54fbd43ba5835d84cf1a79e17dc9154a72eb23
NazdrachVl <vl.nazdrach@hotmail.com>
2017-05-08 18:25:09 +0300

    Initial commit
    version 1

---
+++ testTaskWPF/IHMedicalTask1WPF/IHMedicalTask1WPF/Cell.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHMedicalTask1WPF
+{
+    class Cell
+    {
+        public bool IsQueen { get; set; }
+
+        public CellType Type { get; set; }
+
+        public Cell(CellType type, bool isQueen)
+        {
+            Type = type;
+            IsQueen = isQueen;
+        }
+
+        public Cell(CellType type) : this(type, false) { }
+
+        public Cell() : this(CellType.Empty, false) { }
+
+        public Cell Copy()
+        {
+            return new Cell(Type, IsQueen);
+        }
+    }
+
+    public enum CellType
+    {
+        Empty,
+        White,
+        Black
+    }
+}
